d5f4310324eaf8681f4b2393f4753e26cf52adba
panchuanxiang-svg <panchuanxiang@gmail.com>
2026-05-28 13:59:21 +0800

    Update build.gradle.kts

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,315 +1,45 @@
-import com.codingfeline.buildkonfig.compiler.FieldSpec.Type.STRING
-import org.gradle.kotlin.dsl.androidComponents
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+val commonMain by getting {
+    dependencies {
+        api(libs.compose.foundation)
+        api(libs.compose.material3)
+        api(libs.compose.runtime)
+        api(libs.compose.ui)
+        api(libs.material.icons.core)
+        api(libs.kotlin)
+        api(libs.kotlin.reflect)
+        api(libs.kotlinx.coroutines)
+        api(libs.kotlinx.datetime)
+        api(libs.kotlinx.io.core)
+        api(libs.kotlinx.serialization.json)
+        api(libs.ksoup)
 
-plugins {
-    alias(libs.plugins.bugsnag.gradle)
-    alias(libs.plugins.buildkonfig)
-    alias(libs.plugins.compose)
-    alias(libs.plugins.kotlin.atomicfu)
-    alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.kotlin.multiplatform.android.library)
-    alias(libs.plugins.kotlin.native.cocoapods)
-    alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.moko.resources)
-}
+        // ✅ KTOR（统一 CIO）
+        api(libs.ktor.client.core)
+        api(libs.ktor.client.cio)
+        api(libs.ktor.client.content.negotiation)
+        api(libs.ktor.serialization.kotlinx.json)
+        api(libs.ktor.client.auth)
 
-
-group = rootProject.extra["groupName"].toString()
-version = rootProject.extra["versionName"].toString()
-
-kotlin.sourceSets.all {
-    languageSettings.optIn("kotlin.RequiresOptIn")
-}
-
-val javaVersionEnum: JavaVersion by rootProject.extra
-
-kotlin {
-    jvmToolchain(javaVersionEnum.toString().toInt())
-
-    val iosArm64 = iosArm64()
-    val iosSimulatorArm64 = iosSimulatorArm64()
-
-    val versionCode: Int by rootProject.extra
-    val versionName: String by rootProject.extra
-    val packageName: String by rootProject.extra
-
-    listOf(iosArm64, iosSimulatorArm64).forEach {
-        it.compilations.getByName("main") {
-            cinterops.create("BugsnagSamloader") {
-                includeDirs("$projectDir/src/nativeInterop/cinterop/Bugsnag")
-                definitionFile.set(file("$projectDir/src/nativeInterop/cinterop/Bugsnag.def"))
-            }
-        }
-        it.binaries {
-            framework {
-                isStatic = true
-                binaryOption("bundleVersion", versionCode.toString())
-                binaryOption(
-                    "bundleShortVersionString",
-                    versionName,
-                )
-                binaryOption("bundleId", packageName)
-                export(libs.nsexceptionKt.core)
-            }
-        }
-    }
-
-    android {
-        withJava()
-
-        val compileSdk: Int by rootProject.extra
-        val minSdk: Int by rootProject.extra
-
-        this.compileSdk = compileSdk
-        this.minSdk = minSdk
-
-        androidResources {
-            enable = true
-        }
-
-        namespace = "tk.zwander.common"
-
-        this.enableCoreLibraryDesugaring = true
-
-        compilerOptions {
-            val javaVersionEnum: JavaVersion by rootProject.extra
-            jvmTarget.set(JvmTarget.fromTarget(javaVersionEnum.toString()))
-        }
-
-//        buildFeatures {
-//            aidl = true
-//        }
-
-//        sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-//        sourceSets["main"].res.srcDirs("src/androidMain/res")
-//        sourceSets["main"].res.srcDir(layout.buildDirectory.file("generated/moko/androidMain/res"))
-    }
-
-    jvm {
-        compilations.all {
-            compileTaskProvider.configure {
-                compilerOptions {
-                    jvmTarget = JvmTarget.fromTarget(javaVersionEnum.toString())
-                }
-            }
-        }
-    }
-
-    targets.all {
-        compilations.all {
-            compileTaskProvider.configure {
-                compilerOptions {
-                    freeCompilerArgs.addAll("-Xexpect-actual-classes", "-Xdont-warn-on-error-suppression")
-                }
-            }
-        }
-    }
-
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xexpect-actual-classes", "-Xdont-warn-on-error-suppression")
-    }
-
-    cocoapods {
-        version = versionCode.toString()
-        summary = "Bifrost"
-        homepage = "https://zwander.dev"
-        ios.deploymentTarget = "14.0"
-        osx.deploymentTarget = "10.13"
-        podfile = project.file("../iosApp/Podfile")
-        framework {
-            baseName = "common"
-            isStatic = true
-            export(libs.moko.resources)
-            export(libs.nsexceptionKt.core)
-
-            binaryOption("bundleVersion", versionCode.toString())
-            binaryOption(
-                "bundleShortVersionString",
-                versionName,
-            )
-            binaryOption("bundleId", packageName)
-        }
-    }
-
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                api(libs.compose.foundation)
-                api(libs.compose.material3)
-                api(libs.compose.runtime)
-                api(libs.compose.ui)
-                api(libs.material.icons.core)
-                api(libs.kotlin)
-                api(libs.kotlin.reflect)
-                api(libs.kotlinx.coroutines)
-                api(libs.kotlinx.datetime)
-                api(libs.kotlinx.io.core)
-                api(libs.kotlinx.serialization.json)
-                api(libs.ksoup)
-                api(libs.ktor.client.auth)
-                api(libs.ktor.client.core)
-                api(libs.ktor.client.cio)
-                api(libs.moko.resources)
-                api(libs.moko.resources.compose)
-                api(libs.multiplatformSettings)
-                api(libs.multiplatformSettings.noArg)
-                api(libs.richeditor.compose)
-                api(libs.semver)
-                api(libs.filekit.core)
-                api(libs.filekit.dialogs.compose)
-                api(libs.kmpfile)
-                api(libs.kmpplatform)
-                api(libs.zwander.composedialog)
-                api(libs.zwander.materialyou)
-                api(libs.csv)
-                api(libs.cryptography.core)
-                api(libs.kotlinx.crypto.crc32)
-                api(libs.kotlinx.atomicfu)
-                api(libs.androidx.performance.annotation)
-                api(libs.xmlbuilder)
-                api(libs.ketch.core)
-                api(libs.ketch.ktor)
-                api(libs.ketch.sqlite)
-            }
-        }
-
-        val androidAndJvmMain by creating {
-            dependsOn(commonMain)
-
-            dependencies {
-//                api(libs.ktor.client.okhttp)
-                api(libs.cryptography.provider.jdk)
-            }
-        }
-
-        val skiaMain by creating {
-            dependsOn(commonMain)
-        }
-
-        val jvmMain by getting {
-            dependsOn(androidAndJvmMain)
-            dependsOn(skiaMain)
-
-            dependencies {
-                api(compose.desktop.currentOs)
-                api(libs.bugsnag.jvm)
-                api(libs.flatlaf)
-                api(libs.jna)
-                api(libs.jna.platform)
-                api(libs.jsystemthemedetector)
-                api(libs.kotlinx.coroutines.swing)
-                api(libs.oshi.core)
-                api(libs.slf4j)
-                api(libs.window.styler)
-                api(libs.conveyor.control)
-                api(libs.appdirs)
-            }
-        }
-
-        val androidMain by getting {
-            dependsOn(androidAndJvmMain)
-
-            dependencies {
-                api(libs.androidx.activity.compose)
-                api(libs.androidx.core.ktx)
-                api(libs.androidx.documentfile)
-                api(libs.androidx.preference.ktx)
-                api(libs.bugsnag.android)
-                api(libs.google.material)
-                api(libs.kotlinx.coroutines.android)
-                api(libs.github.api)
-                api(libs.filepicker)
-            }
-        }
-
-        val darwinMain by creating {
-            dependsOn(skiaMain)
-            dependencies {
-//                api(libs.ktor.client.darwin)
-                api(libs.nsexceptionKt.core)
-                api(libs.nserrorKt)
-                api(libs.cryptography.provider.openssl3.prebuilt)
-                api(libs.ktor.client.darwin)
-            }
-        }
-
-        val iosArm64Main by getting {
-            resources.srcDirs("build/generated/moko/iosArm64Main/src")
-        }
-        val iosSimulatorArm64Main by getting {
-            resources.srcDirs("build/generated/moko/iosSimulatorArm64Main/src")
-        }
-        val iosMain by creating {
-            dependsOn(darwinMain)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-        }
-    }
-}
-
-androidComponents {
-    onVariants { variant ->
-        variant.sources.res?.addStaticSourceDirectory("generated/moko/androidMain/res")
-    }
-}
-
-buildkonfig {
-    packageName = "tk.zwander.common"
-    objectName = "GradleConfig"
-    exposeObjectWithName = objectName
-
-    defaultConfigs {
-        buildConfigField(STRING, "versionName", "${rootProject.extra["versionName"]}")
-        buildConfigField(STRING, "versionCode", "${rootProject.extra["versionCode"]}")
-        buildConfigField(STRING, "appName", "${rootProject.extra["appName"]}")
-        buildConfigField(STRING, "bugsnagJvmApiKey", "${rootProject.extra["bugsnagJvmApiKey"]}")
-        buildConfigField(STRING, "bugsnagAndroidApiKey", "${rootProject.extra["bugsnagAndroidApiKey"]}")
-    }
-}
-
-multiplatformResources {
-    resourcesPackage.set("tk.zwander.samloaderkotlin.resources")
-}
-
-tasks.withType<Copy> {
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-}
-
-dependencies {
-    coreLibraryDesugaring(libs.desugar.jdk.libs)
-}
-
-afterEvaluate {
-    val versionName: String by rootProject.extra
-    val versionCode: Int by rootProject.extra
-
-    try {
-        providers.exec {
-            commandLine(
-                "plutil",
-                "-replace",
-                "CFBundleShortVersionString",
-                "-string",
-                versionName,
-                "../iosApp/iosApp/Info.plist",
-            )
-        }
-    } catch (_: Throwable) {
-    }
-
-    try {
-        providers.exec {
-            commandLine(
-                "plutil",
-                "-replace",
-                "CFBundleVersion",
-                "-string",
-                "$versionCode",
-                "../iosApp/iosApp/Info.plist",
-            )
-        }
-    } catch (_: Throwable) {
+        api(libs.moko.resources)
+        api(libs.moko.resources.compose)
+        api(libs.multiplatformSettings)
+        api(libs.multiplatformSettings.noArg)
+        api(libs.richeditor.compose)
+        api(libs.semver)
+        api(libs.filekit.core)
+        api(libs.filekit.dialogs.compose)
+        api(libs.kmpfile)
+        api(libs.kmpplatform)
+        api(libs.zwander.composedialog)
+        api(libs.zwander.materialyou)
+        api(libs.csv)
+        api(libs.cryptography.core)
+        api(libs.kotlinx.crypto.crc32)
+        api(libs.kotlinx.atomicfu)
+        api(libs.androidx.performance.annotation)
+        api(libs.xmlbuilder)
+        api(libs.ketch.core)
+        api(libs.ketch.ktor)
+        api(libs.ketch.sqlite)
     }
 }
