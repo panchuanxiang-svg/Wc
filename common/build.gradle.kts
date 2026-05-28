1205246e4bb09dc7c639887210aca2928faf1e79
panchuanxiang-svg <panchuanxiang@gmail.com>
2026-05-28 19:51:06 +0800

    Update build.gradle.kts

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,85 +1,29 @@
 plugins {
-    id("org.jetbrains.kotlin.multiplatform")
+    kotlin("multiplatform")
     id("com.android.library")
-    id("org.jetbrains.kotlin.plugin.serialization")
-    id("org.jetbrains.compose")
-    id("org.jetbrains.kotlin.plugin.compose") 
 }
 
 kotlin {
-    // 配置多平台共享模块编译 Android 库时的 target 级别
-    androidTarget {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+    androidTarget()
+    jvm {
+        compilations.all {
+            compilerOptions.configure {
+                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+            }
         }
     }
-
-    // 同步配置 JVM (Desktop) 端的编译器目标级别为 21
-    jvm("desktop") {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
-        }
-    }
-
-    iosArm64()
-    iosSimulatorArm64()
-
+    
     sourceSets {
-
-        val commonMain by getting {
-            dependencies {
-                implementation(compose.runtime)
-                implementation(compose.foundation)
-                implementation(compose.material3)
-                implementation(compose.ui)
-
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
-
-                implementation("io.ktor:ktor-client-core:2.3.12")
-                implementation("io.ktor:ktor-client-content-negotiation:2.3.12")
-                implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.12")
-                implementation("io.ktor:ktor-client-auth:2.3.12")
-
-                implementation("com.mohamedrejeb.ksoup:ksoup:0.2.1")
-            }
+        commonMain.dependencies {
+            // 修复：更新至最新维护版本，解决依赖解析报错
+            implementation("com.fleeksoft.ksoup:ksoup:0.2.6")
         }
-
-        val androidMain by getting {
-            dependencies {
-                implementation("io.ktor:ktor-client-okhttp:2.3.12")
-                implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.21")
-            }
-        }
-
-        val desktopMain by getting {
-            dependencies {
-                implementation("io.ktor:ktor-client-cio:2.3.12")
-                implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.21")
-            }
-        }
-
-        val iosMain by creating {
-            dependsOn(commonMain)
-            dependencies {
-                implementation("io.ktor:ktor-client-darwin:2.3.12")
-            }
-        }
-        
-        val iosArm64Main by getting { dependsOn(iosMain) }
-        val iosSimulatorArm64Main by getting { dependsOn(iosMain) }
     }
 }
 
 android {
     namespace = "tk.zwander.common"
     compileSdk = 34
-
-    defaultConfig {
-        minSdk = 26
-    }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
