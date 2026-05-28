7551089bc5983d1896da692202eb028a20876954
panchuanxiang-svg <panchuanxiang@gmail.com>
2026-05-28 20:13:34 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,37 +1,21 @@
 pluginManagement {
     repositories {
-        gradlePluginPortal()
-        mavenCentral()
         google()
-
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/")
-        maven("https://maven.hq.hydraulic.software")
+        mavenCentral()
+        gradlePluginPortal()
     }
-}
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    plugins {
+        id("org.jetbrains.kotlin.multiplatform") version "2.0.21"
+        id("org.jetbrains.kotlin.android") version "2.0.21"
+        id("org.jetbrains.compose") version "1.6.11"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
+    }
 }
 
 dependencyResolutionManagement {
-    // ✅ 已修复：将 FAIL_ON_PROJECT_REPOS 改为 PREFER_SETTINGS
-    // 允许子模块（如 :android）声明自己的独占仓库，防止 Android 插件解析链直接崩溃断开
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS) 
-    
     repositories {
-        mavenCentral()
         google()
-
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/")
-        maven("https://maven.pkg.jetbrains.space/public/p/ktor/eap/")
-        maven("https://jitpack.io")
-        maven("https://repo.jenkins-ci.org/public/")
+        mavenCentral()
     }
 }
-
-rootProject.name = "SamloaderKotlin"
-include(":android")
-include(":desktop")
-include(":common")
